ea983cafe07685eed1e3bd9eb65889847e2ba846
Akulik <Akulik03@icloud.com>
2024-03-14 19:40:45 +0200

    MutableLiveData

--- app/src/main/java/com/example/homework52/MainActivity.java
+++ app/src/main/java/com/example/homework52/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
     private EditText editText;
     private Button buttonChange;
     private MainViewModel viewModel;
-    private static final String KEY_NAME = "name";
     private int count = 0;
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,37 +30,32 @@ public class MainActivity extends AppCompatActivity {
         textView = findViewById(R.id.textView2);
         editText = findViewById(R.id.editTextText);
         buttonChange = findViewById(R.id.button2);
-
-        if (savedInstanceState != null) {
-            String savedName = savedInstanceState.getString(KEY_NAME);
-            if (savedName != null && !savedName.isEmpty()) {
-                viewModel.setName(savedName);
-                textView.setText("Привет, " + viewModel.getName() + "!");
-            }
-        }
-
         buttonCounter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                count++;
-                textViewCounter.setText("Количество кликов: " + count);
+                viewModel.incrementClickCount();
             }
         });
-
         buttonChange.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String name = editText.getText().toString();
                 if (!name.isEmpty()) {
                     viewModel.setName(name);
-                    textView.setText("Привет, " + viewModel.getName() + "!");
                 }
             }
         });
-    }
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putString(KEY_NAME, viewModel.getName());
+        viewModel.getName().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String newName) {
+                textView.setText("Привет, " + newName + "!");
+            }
+        });
+        viewModel.getClickCount().observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer newCount) {
+                textViewCounter.setText("Количество кликов: " + newCount);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/homework52/MainViewModel.java
+++ app/src/main/java/com/example/homework52/MainViewModel.java
@@ -4,15 +4,21 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MainViewModel extends ViewModel {
-   // public String name = "ananimus";
-    //public MutableLiveData<String> name = new MutableLiveData<>("ananimus");
-   private String name = "";
+    public MutableLiveData<String> name = new MutableLiveData<>();
+    private MutableLiveData<Integer> clickCount = new MutableLiveData<>();
 
-    public String getName() {
-        return name;
+    public void setName(String newName) {
+        name.setValue(newName);
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public MutableLiveData<String> getName() {
+        return name;
+    }
+    public void incrementClickCount() {
+        clickCount.setValue(clickCount.getValue() != null ? clickCount.getValue() + 1 : 1);
+    }
+
+    public MutableLiveData<Integer> getClickCount() {
+        return clickCount;
     }
 }
